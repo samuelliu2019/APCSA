ccbb3bba9b12fab9bfe61ae55d1f41621eeeb409
samuelliu2019 <samuelliu2019@gmail.com>
2018-03-05 11:13:52 -0800

    Start MadLib

---
+++ Unit10/src/MadLib.java
@@ -0,0 +1,124 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class MadLib
+{
+	private ArrayList<String> verbs;
+	private ArrayList<String> nouns;
+	private ArrayList<String> adjectives;
+	
+	public MadLib()
+	{
+		verbs = new ArrayList<String>();
+		nouns = new ArrayList<String>();
+		adjectives = new ArrayList<String>();
+
+
+	}
+
+	public MadLib(String fileName)
+	{
+		this();
+		
+		loadNouns();
+		loadVerbs();
+		loadAdjectives();
+		//load stuff
+		
+		
+		
+		try{
+			Scanner file = new Scanner(new File(fileName));
+		
+			
+		
+		
+		
+		
+		
+	
+		
+		}
+		catch(Exception e)
+		{
+			out.println("Houston we have a problem!");
+		}
+		
+	}
+
+	public void loadNouns()
+	{
+		try{
+		
+		
+		
+		
+		
+		}
+		catch(Exception e)
+		{
+		}	
+		
+	}
+	
+	public void loadVerbs()
+	{
+		try{
+	
+	
+	
+	
+	
+		}
+		catch(Exception e)
+		{
+		}
+	}
+
+	public void loadAdjectives()
+	{
+		try{
+	
+	
+	
+	
+	
+		}
+		catch(Exception e)
+		{
+		}
+	}
+
+	public String getRandomVerb()
+	{
+	
+		return "";
+	}
+	
+	public String getRandomNoun()
+	{
+		
+		return "";
+	}
+	
+	public String getRandomAdjective()
+	{
+		
+		return "";
+	}		
+
+	public String toString()
+	{
+	   return "\n\n\n";
+	}
+}
