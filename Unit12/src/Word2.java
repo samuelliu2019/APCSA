82766397caf496dd9db258fc1d33063aaf862843
samuelliu2019 <samuelliu2019@gmail.com>
2018-03-15 11:11:27 -0700

    Unit12

---
+++ Unit12/src/Word2.java
@@ -0,0 +1,47 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Word2 implements Comparable<Word2>
+{
+	private String word;
+
+	public Word2( String s)
+	{
+
+
+	}
+
+	private int numVowels()
+	{
+		String vowels = "AEIOUaeiou";
+		int vowelCount=0;
+
+
+
+
+
+
+
+		return vowelCount;
+	}
+
+	public int compareTo(Word2 rhs)
+	{
+
+
+
+	
+		return -1;
+	}
+
+	public String toString()
+	{
+		return word;
+	}
+ 
+}
