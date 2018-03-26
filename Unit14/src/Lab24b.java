c3b946aa506eef95d9dd99beec0724e1e92075dc
samuelliu2019 <samuelliu2019@gmail.com>
2018-03-26 10:06:49 -0700

    Unit14

--- Unit14/src/Lab24b.java
+++ Unit14/src/Lab24b.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -29,66 +27,4 @@ public class Lab24b
 
 	   }
 	}
-=======
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class - 
-//Lab  -
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-import static java.lang.System.*;
-
-public class Lab24b
-{
-	public static void main( String args[] ) throws IOException
-	{
-		FancyWord test = new FancyWord();
-		Scanner file = new Scanner(new File ("C:\\Users\\leuma\\Downloads\\Unit14_2016\\Unit14-Assignments\\lab24b.dat"));
-		int size = file.nextInt();
-		file.nextLine();
-		for(int i = 0; i<size; i++)
-		{
-			String word = file.next();
-			test.setFancyWord(word);
-			System.out.println(test);
-
-
-
-	   }
-	}
->>>>>>> origin/master
-=======
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class - 
-//Lab  -
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-import static java.lang.System.*;
-
-public class Lab24b
-{
-	public static void main( String args[] ) throws IOException
-	{
-		FancyWord test = new FancyWord();
-		Scanner file = new Scanner(new File ("C:\\Users\\leuma\\Downloads\\Unit14_2016\\Unit14-Assignments\\lab24b.dat"));
-		int size = file.nextInt();
-		file.nextLine();
-		for(int i = 0; i<size; i++)
-		{
-			String word = file.next();
-			test.setFancyWord(word);
-			System.out.println(test);
-
-
-
-	   }
-	}
->>>>>>> origin/master
 }
