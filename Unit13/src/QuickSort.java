2626624cf57754536dcef4b793a6f769da141eb7
samuelliu2019 <samuelliu2019@gmail.com>
2018-03-29 11:08:09 -0700

    Unit13

--- Unit13/src/QuickSort.java
+++ Unit13/src/QuickSort.java
@@ -13,16 +13,21 @@ public class QuickSort
 
 	public static void quickSort(Comparable[] list)
 	{
-
-
-
+		passCount = 0;
+		quickSort(list,0,list.length-1);
+		System.out.println(Arrays.toString(list));
 
 	}
 
 
 	private static void quickSort(Comparable[] list, int low, int high)
 	{
-
+		if(low<high)
+		{
+			int p = partition(list,low,high);
+			quickSort(list,low,p);
+			quickSort(list,p+1,high);
+		}
 
 
 
@@ -34,20 +39,22 @@ public class QuickSort
 
 	private static int partition(Comparable[] list, int low, int high)
 	{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		return 0;
+		  Comparable pivot = list[low];
+		  int bot = low-1;
+		  int top = high+1;       
+		  while(bot<top)
+		  {
+		  while (list[--top].compareTo(pivot) > 0);
+		  while (list[++bot].compareTo(pivot) < 0);
+		  if(bot >= top)
+		  {
+		        return top;
+		  }
+		  Comparable temp = list[bot];
+		  list[bot] = list[top];
+		  list[top] = temp;
+	      }
+		  return 0;
 	}
+
 }
---
+++ Unit15/src/Block.java
@@ -0,0 +1,116 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Block implements Locatable
+{
+	private int xPos;
+	private int yPos;
+	private int width;
+	private int height;
+
+	private Color color;
+
+	public Block()
+	{
+
+
+	}
+
+	//add other Block constructors - x , y , width, height, color
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+   //add the other set methods
+   
+
+   public Block(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+public Block(int i, int j, int k, int l) {
+	// TODO Auto-generated constructor stub
+}
+
+public Block(int i, int j, int k, int l, Color red) {
+	// TODO Auto-generated constructor stub
+}
+
+public void setColor(Color col)
+   {
+
+
+   }
+
+   public void draw(Graphics window)
+   {
+   	//uncomment after you write the set and get methods
+      //window.setColor(color);
+      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+   }
+
+   public void draw(Graphics window, Color col)
+   {
+
+
+   }
+   
+	public boolean equals(Object obj)
+	{
+
+
+
+
+		return false;
+	}
+
+	@Override
+	public void setPos(int x, int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setX(int x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setY(int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}   
+
+   //add the other get methods
+    
+
+   //add a toString() method  - x , y , width, height, color
+}
